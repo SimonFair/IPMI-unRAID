9009b11524936f902524057d1f5dae6bcf60bab3
SimonFair <39065407+SimonFair@users.noreply.github.com>
2023-12-03 08:02:45 +0000

    Add board_model

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_check.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_check.php
@@ -2,5 +2,6 @@
 /* board info */
 $boards = ['ASRock'=>'','ASRockRack'=>'', 'Dell' =>'','Supermicro'=>''];
 $board  = ( $override == 'disable') ? trim(shell_exec("dmidecode -t 2 | grep 'Manufacturer' | awk -F 'r:' '{print $2}' | awk  '{print $1}'")) : $oboard;
+$board_model = ( $override == 'disable') ? rtrim(ltrim(shell_exec("dmidecode -qt2|awk -F: '/^\tProduct Name:/ {print $2}'"))) : $omodel;
 $board_status  = array_key_exists($board, $boards);
 ?>
